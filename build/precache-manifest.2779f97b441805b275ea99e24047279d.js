f3fa8c1c97be6dd734390b578fb9cb15429ec709
pichuca <pichucasystems@gmail.com>
2019-09-28 12:01:44 -0300

    Moved build folder to root

---
+++ build/precache-manifest.2779f97b441805b275ea99e24047279d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f58ef04b23323cadcc75b88cd89eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d4531912dd580d2b4f3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f8fbf594cf3c95ac6f2",
+    "url": "/static/js/2.e001df75.chunk.js"
+  },
+  {
+    "revision": "8d4531912dd580d2b4f3",
+    "url": "/static/js/main.239f00d3.chunk.js"
+  },
+  {
+    "revision": "c5563521c846e2b2bb0b",
+    "url": "/static/js/runtime-main.e2fca4a6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
