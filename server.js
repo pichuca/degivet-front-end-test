e15865a1673de6c6d3ef248a7d9bc72544480565
pichuca <pichucasystems@gmail.com>
2019-09-28 11:50:49 -0300

    Move server file to root git folder for deployment purposes

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const path = require('path');
 const port = process.env.PORT || 5200;
 const app = express();
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/degivet-reddit-top-50/build/favicon.ico'));
 // The __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '/degivet-reddit-top-50/build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/degivet-reddit-top-50/build', '/degivet-reddit-top-50/index.html'));
 });
 
 // Listen.
