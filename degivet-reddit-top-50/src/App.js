d29480ec76c191495698a1d21d6bc12567183878
pichuca <pichucasystems@gmail.com>
2019-09-29 19:35:00 -0300

    Changed header text content

--- degivet-reddit-top-50/src/App.js
+++ degivet-reddit-top-50/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Welcome to Reddit's top 50, YAY!
+        Degivet Reddit's top 50
       </header>
     </div>
   );
