3c21eb6a20fc17b70fe7e84bb8e2b161c4f961c2
pichuca <pichucasystems@gmail.com>
2019-12-28 12:15:50 -0300

    Configure store

--- degivet-reddit-top-50/src/index.js
+++ degivet-reddit-top-50/src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers';
+import loggerMiddleware from './middleware/logger';
+
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware);
+const composedEnhancers = compose(middlewareEnhancer); // enhancers can be passed as comma separated params into compose() function.
+
+const store = createStore(rootReducer, undefined, composedEnhancers);
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
