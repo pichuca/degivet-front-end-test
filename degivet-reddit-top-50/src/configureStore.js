5e05ace8ba05d73c74dd4878d4aa53bf288a1673
pichuca <pichucasystems@gmail.com>
2019-12-28 13:06:36 -0300

    Use redux toolkit to reduce store config complexity

--- degivet-reddit-top-50/src/configureStore.js
+++ degivet-reddit-top-50/src/configureStore.js
@@ -1,19 +1,16 @@
-import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import hotReloading from './utils/hotReloading';
 
 import loggerMiddleware from './middleware/logger'
 import rootReducer from './reducers'
 
-export default function configureStore(preloadedState) {
-    const middlewares = [loggerMiddleware, thunkMiddleware];
-    const middlewareEnhancer = applyMiddleware(...middlewares);
-
-    const enhancers = [middlewareEnhancer];
-    const composedEnhancers = composeWithDevTools(...enhancers);
-
-    const store = createStore(rootReducer, preloadedState, composedEnhancers);
+export default function configureAppStore(preloadedState) {
+    const store = configureStore({
+        reducer: rootReducer,
+        middleware: [loggerMiddleware, ...getDefaultMiddleware()],
+        preloadedState,
+        enhancers: []
+    });
 
     // hot reloading
     hotReloading('./reducers', () => store.replaceReducer(rootReducer));
