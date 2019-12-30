fb8c626159b03f82952a25dc8daa187cf86b069e
pichuca <pichucasystems@gmail.com>
2019-12-30 13:16:12 -0300

    Make posts list component class, added pagination package

--- degivet-reddit-top-50/src/components/posts-list/posts-list.js
+++ degivet-reddit-top-50/src/components/posts-list/posts-list.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 import Post from '../post/post';
 
 import List from '@material-ui/core/List';
 
-function PostsList(props) {
-    const postItems = props.posts;
-    const postsElements = postItems ? postItems.map((postData) => {
-        return <Post data={postData.data} key={postData.data.id}/>;
-    }) : null;
-    return (
-        <List>{postsElements}</List>
-    );
+class PostsList extends React.Component  {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        const postItems = this.props.posts;
+        const postsElements = postItems ? postItems.map((postData) => {
+            return <Post data={postData.data} key={postData.data.id}/>;
+        }) : null;
+        return (
+            <List className={'list'}>{postsElements}</List>
+        );
+    }
 }
 
 export default PostsList;
